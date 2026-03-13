eee926e07130a3b20f40cea6de53fc7294bd09bf
Omkar Balasaheb Deshmukh <167670795+OmkarDeshmukh09@users.noreply.github.com>
2026-03-13 14:29:18 +0530

    Program 8 of FTP Server

---
+++ program673.c
@@ -0,0 +1,82 @@
+///////////////////////////////////////////////////////////////////////////
+//
+//                              CLIENT CODE
+//
+///////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>                  // printf, scanf
+#include <stdlib.h>                 // malloc, free  
+#include <string.h>                 // memset, memcmp 
+
+#include <unistd.h>                 // read, write, close                 
+#include <fcntl.h>                  // creat, unlink
+
+#include <sys/socket.h>             //scoket, bind, listen, accept, connect
+#include <netinet/in.h>             // sockaddr_in, htons
+
+#include <arpa/inet.h>
+
+int main()
+{
+    int ServerSocketfd = 0 ;
+    int iRet = 0 ;
+    int Port = 11000;
+
+    struct sockaddr_in ServerAddr;
+
+    char Buffer [1024] = {'\0'};
+
+//  Step 1 : Create TCP socket
+
+    ServerSocketfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (ServerSocketfd < 0)
+    {
+        printf("Unable to Create the Socket \n");
+        return -1;
+    }
+
+    printf("Socket gets successfully with fd : %d\n",ServerSocketfd);    
+ 
+//  Step 2 : Connect with the Server
+
+    memset(&ServerAddr, 0, sizeof(ServerAddr));
+
+    ServerAddr.sin_family = AF_INET;
+    ServerAddr.sin_port = htons(Port);
+
+//  127.0.0.1 -> Binary form
+    
+    inet_pton(AF_INET,"127.0.0.1",&ServerAddr.sin_addr);
+
+    iRet = connect(ServerSocketfd, (struct sockaddr*) &ServerAddr, sizeof(ServerAddr));
+
+    if (iRet == -1)
+    {
+        printf("Unablr to connect with server\n");
+        return -1;
+    }
+
+    printf("Client Succesfully connected with Server\n");
+    
+//  Step 3 : Read the Data from Server
+
+    iRet = read(ServerSocketfd, Buffer, sizeof(Buffer) -1);
+
+    if (iRet <= 0)
+    {
+        printf("Unable to read the Data from Server\n");
+        return -1;
+    }
+
+    printf("Data from server is %s\n",Buffer);
+
+//  Step 4 : Close all the resources 
+    
+    close(ServerSocketfd);
+    
+    
+    printf("Terminating the server application\n");
+
+    return 0 ;
+}

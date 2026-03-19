b3e2d9230a25481cf1e45510b956ae56914b2654
Omkar Balasaheb Deshmukh <167670795+OmkarDeshmukh09@users.noreply.github.com>
2026-03-19 15:23:08 +0530

    Program 15 of FTP Server

---
+++ program680.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main()
+{
+    printf("PID of the runiing process : %d\n",getpid);
+
+    printf("PID of the runiing process : %d\n",getppid);
+
+    return 0 ;
+}

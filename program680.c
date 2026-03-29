af8e4b4ca3030492b597682ba9506917f57c35a9
Omkar Balasaheb Deshmukh <167670795+OmkarDeshmukh09@users.noreply.github.com>
2026-03-29 22:54:16 +0530

    Updated program680.c

--- program680.c
+++ program680.c
@@ -4,7 +4,7 @@
 
 int main()
 {
-    printf("PID of the runiing process : %d\n",getpid);
+    printf("PID of the runiing process : %d \n",getpid);
 
     printf("PID of the runiing process : %d\n",getppid);
 

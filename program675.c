79737836ab954f947fa941711e4ae1ebc4f0e2a6
Omkar Balasaheb Deshmukh <167670795+OmkarDeshmukh09@users.noreply.github.com>
2026-04-05 23:52:10 +0530

    Update program675.c

--- program675.c
+++ program675.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 
     printf(" Executable name : %s\n",argv[0]);
     printf(" IP Address : %s\n",argv[1]);
-    printf(" Port Number :%s\n",argv[2]);
+    printf(" Port Number : %s\n",argv[2]);
     printf(" Targated file name : %s\n",argv[3]);
     printf(" New file Name : %s\n",argv[4]);
     
